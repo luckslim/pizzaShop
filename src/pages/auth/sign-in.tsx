f0a72ad191c06b6e7e83c4c0d475f48b4002c6b6
Lucas <lucaslima78@hotmail.com>
2025-02-06 07:10:05 -0300

    up

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,28 @@
+import { Button } from "@/components/components/ui/button";
+import { Input } from "@/components/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
 export function SignIn(){
     return(
-        <h1>Sign-in</h1>
-    )
+        <>
+        <div className="p-8">
+            <div className="flex w-[350px] flex-col justify-center gap-6">
+                <div className="flex flex-col gap-2 text-center">
+                    <h1 className="text-2xl font-semibold tracking-tight">Acessar Painel</h1>
+                    <p className="text-sm text-muted-foreground">
+                        Acompanhe suas vendas pelo painel do parceiro!
+                    </p>
+                </div>
+                <form className="space-y-4">
+                    <div className="space-y-2">
+                       <Label htmlFor="email">Seu email</Label> 
+                       <Input id="email" type="email"></Input>
+                    </div>
+                    <Button className="w-full" type="submit" >Acessar Painel</Button>
+                </form>
+
+            </div>
+        </div>
+        </>
+         )
 }
