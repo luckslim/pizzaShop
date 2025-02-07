b6eae326366fd2fde3165c8c4c1568ea63200492
Lucas SL <newlucaslima78@gmail.com>
2025-02-07 16:57:34 -0300

    up

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,15 @@
+export function Orders() {
+    return (
+        <>
+            <div className="flex flex-col gap-4">
+                <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+            </div>
+            <div className="space-y-2.5">
+                <form className="flex items-center gap-2">
+                    <span className="text-sm font-semibold">Filtros:</span>
+                </form>
+            </div>
+        </>
+
+    )
+}
