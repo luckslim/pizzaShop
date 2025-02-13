019877107c94235bff8adb6a8efe930250c5c829
Lucas SL <newlucaslima78@gmail.com>
2025-02-13 12:03:42 -0300

    up

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/components/ui/button";
+import { Input } from "@/components/components/ui/input";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/components/ui/table";
+import { ArrowRight, Ghost, Search, X } from "lucide-react";
+
 export function Orders() {
     return (
         <>
@@ -7,7 +12,62 @@ export function Orders() {
             <div className="space-y-2.5">
                 <form className="flex items-center gap-2">
                     <span className="text-sm font-semibold">Filtros:</span>
+                    <Input placeholder="Nome do Cliente" className="h-8 w-[320px]"></Input>
                 </form>
+            </div>
+            <div className="border rounded-md">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[64px]"></TableHead>
+                            <TableHead className="w-[148px]">Identificador</TableHead>
+                            <TableHead className="w-[180px]">Realizado há</TableHead>
+                            <TableHead className="w-[148px]">Status</TableHead>
+                            <TableHead>Cliente</TableHead>
+                            <TableHead className="w-[148px]">Total do Pedido</TableHead>
+                            <TableHead className="w-[164px]"></TableHead>
+                            <TableHead className="w-[132px]"></TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                <Button variant="outline" size="xs">
+                                    <Search className="h-3 w-3">
+                                        <span className="sr-only">Detalhes do pedido</span>
+                                    </Search>
+                                </Button>
+                            </TableCell>
+                            <TableCell className="font-mono text-xs font-medium">8as9fy8f09yfsddsf</TableCell>
+                            <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+                            <TableCell>
+                                <div className="flex items-center gap-2">
+                                    <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+                                    <span className="font-medium text-muted-foreground">Pendente</span>
+                                </div>
+                            </TableCell>
+                            <TableCell className="font-medium">
+                                lucas soares
+                            </TableCell>
+                            <TableCell className="font-medium">
+                                    R$ 149,98
+                            </TableCell>
+                            <TableCell>
+                                <Button variant="ghost" size="xs">
+                                    <ArrowRight className="mr-2 h-3 w-3"/>
+                                    Aprovar
+                                </Button>
+                            </TableCell>
+                            <TableCell>
+                                <Button variant="ghost" size="xs">
+                                    <X className="mr-2 h-3 w-3"/>
+                                    Cancelar
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+
             </div>
         </>
 
