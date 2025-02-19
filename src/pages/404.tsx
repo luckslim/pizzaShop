3c61503ed2bf2c85d46d9013cde733b9a5bf9426
Lucas <lucaslima78@hotmail.com>
2025-02-19 05:22:25 -0300

    up

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export function NotFound(){
+    return(
+        <div className="flex h-screen flex-col items-center justify-center gap-2">
+            <h1 className="text-4xl font-bold">Pagina não encontrada</h1>
+            <p className="text-accent-foreground">
+                voltar para o <Link to="/">Dashboard</Link>
+            </p>
+
+        </div>
+    )
+}
