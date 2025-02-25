9c83035efde7dac4a9c8a482ee931f35a1a16316
lucaslima <lucaslima78@hotmail.com>
2025-02-25 06:10:29 -0300

    up

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,9 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
 import { Button } from "./components/ui/button";
 import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./components/ui/dialog";
+import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
+import { Textarea } from "./components/ui/textarea";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
 
-
+const storeProfileSchema = z.object({
+    name: z.string().min(1),
+    description: z.string(),
+})
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>
 export function StoreProfileDialog() {
+    const { data: managedRestaurant} = useQuery({
+        queryKey: ['manager-restaurant'],
+        queryFn: getManagedRestaurant,
+    })
+    const {
+        register,
+        handleSubmit,
+    }= useForm<StoreProfileSchema>({
+        resolver: zodResolver(storeProfileSchema),
+        values: {
+            name: managedRestaurant?.name ?? '',
+            description: managedRestaurant?.description ?? '',
+        }
+
+    })
+    
     return (
         <DialogContent>
             <DialogHeader>
@@ -15,8 +42,12 @@ export function StoreProfileDialog() {
             <form action="">
                 <div className="space-y-4 py-4">
                     <div className="grid grid-cols-4 items-center gap-4">
-                        <Label className="text-"></Label>
-
+                        <Label className="text-right" htmlFor="name">Nome</Label>
+                        <Input type="text" className="col-span-3" id="name" {...register('name')}/>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label className="text-right" htmlFor="description">Descrição</Label>
+                        <Textarea className="col-span-3" id="description" {...register('description')}></Textarea>
                     </div>
                 </div>
                 <DialogFooter>
