223931b3c37ae0e64afe6fde554e85f232748b30
lucaslima <lucaslima78@hotmail.com>
2025-03-02 10:11:43 -0300

    up

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,6 +1,9 @@
 import { api } from "@/components/lib/axios";
 export interface GetOrdersQuery{
     pageIndex?:number | null
+    orderId?: string | null
+    customerName?: string | null
+    status?: string | null
 }
 export interface GetOrdersResponse{
     orders: {
@@ -17,11 +20,13 @@ export interface GetOrdersResponse{
     };
 
 }
-export async function getOrders({pageIndex}: GetOrdersQuery){
+export async function getOrders({pageIndex, orderId, customerName, status}: GetOrdersQuery){
     const response = await api.get<GetOrdersResponse>('/orders', {
         params:{
             pageIndex,
-
+            orderId,
+            customerName,
+            status
         }
     })
     return response.data
